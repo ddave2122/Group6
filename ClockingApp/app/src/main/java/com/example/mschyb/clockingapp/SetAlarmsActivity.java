b7a8fee154007c7cbfecb046915c4ad82ee5a10c
Chy-QA <cbrown183@student.gsu.edu>
2015-11-06 23:56:36 -0500

    Added initial code for mobile app
    Added login screen with shared preferences and home screen with
    functionality

---
+++ ClockingApp/app/src/main/java/com/example/mschyb/clockingapp/SetAlarmsActivity.java
@@ -0,0 +1,57 @@
+package com.example.mschyb.clockingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class SetAlarmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_alarms);
+
+        if(SaveSharedPreference.getUserName(getApplicationContext()).length() == 0)
+        {
+            startActivity(new Intent(getApplicationContext(), LoginScreenActivity.class));
+        }
+        else
+        {}
+
+        Button backButton = (Button) findViewById(R.id.backButton);
+        //set the onClick listener for the button
+        backButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
+              }
+          }
+        );//end backButton.setOnClickListener
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_set_alarms, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

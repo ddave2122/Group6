86624d6746011fecdfb0a8e1dbd2970d88406de3
DevShellz <sheldongray85@gmail.com>
2015-11-09 19:58:51 -0500

    Create Schedule Page Added
    The page has been updated to show weeks of the month. Pulls employees
    from the DB and can select start and end dates and times. Does not
    insert data into DB yet.

--- website/include/bottomwrapper.php
+++ website/include/bottomwrapper.php
@@ -43,6 +43,15 @@
     });
 
   });
+
+  /* Schedule Tabs */
+  $('.btnNext').click(function(){
+    $('.nav-tabs > .active').next('li').find('a').trigger('click');
+  });
+
+  $('.btnPrevious').click(function(){
+    $('.nav-tabs > .active').prev('li').find('a').trigger('click');
+  });
 </script>
 
 <!-- Bootstrap Core JavaScript -->

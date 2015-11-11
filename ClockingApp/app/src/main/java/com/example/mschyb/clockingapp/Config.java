31ec5d764553941281e8648154775a4931137f28
David Gibbs <ddave2122@gmail.com>
2015-11-11 01:25:49 -0500

    GPS service is framed out and need to pull GPS data from login

--- ClockingApp/app/src/main/java/com/example/mschyb/clockingapp/Config.java
+++ ClockingApp/app/src/main/java/com/example/mschyb/clockingapp/Config.java
@@ -5,5 +5,67 @@ public class Config
     public final static String TAG = "Group6";
     public final static String ENDPOINT = "http://group6project.com/api/";
     public final static String CHECK_CREDENTIALS_ENDPOINT = ENDPOINT + "checkcredentials.php";
+    public final static String LOG_TIME_ENDPOINT = ENDPOINT + "logtime.php";
+
+    //GPS bounding box
+    private static double eastEndpoint = 0;
+    private static double westEndpoint = 0;
+    private static double southEndpoint = 0 ;
+    private static double northEndpoint = 0;
+
+    private static int userId;
+
+    /**
+     * Sanity check to see if the GPS is still set at default values
+     * @return true if anything is default
+     */
+    public static boolean isGPSDefault()
+    {
+        return(eastEndpoint == 0
+                || westEndpoint == 0
+                || southEndpoint == 0
+                || northEndpoint == 0);
+    }
+
+
+    public static int getUserId() {
+        return userId;
+    }
+
+    public static void setUserId(int userId) {
+        Config.userId = userId;
+    }
+
+    public static double getEastEndpoint() {
+        return eastEndpoint;
+    }
+
+    public static void setEastEndpoint(double eastEndpoint) {
+        Config.eastEndpoint = eastEndpoint;
+    }
+
+    public static double getWestEndpoint() {
+        return westEndpoint;
+    }
+
+    public static void setWestEndpoint(double westEndpoint) {
+        Config.westEndpoint = westEndpoint;
+    }
+
+    public static double getSouthEndpoint() {
+        return southEndpoint;
+    }
+
+    public static void setSouthEndpoint(double southEndpoint) {
+        Config.southEndpoint = southEndpoint;
+    }
+
+    public static double getNorthEndpoint() {
+        return northEndpoint;
+    }
+
+    public static void setNorthEndpoint(double northEndpoint) {
+        Config.northEndpoint = northEndpoint;
+    }
 
 }

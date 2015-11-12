50ba4831c3d7b89da8dea2fc04bed80576723289
DevShellz <sheldongray85@gmail.com>
2015-11-12 11:07:49 -0500

    Web App Update
    Blah Blah

--- website/include/topnavbar.php
+++ website/include/topnavbar.php
@@ -207,11 +207,11 @@
         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
     </a>
     <ul class="dropdown-menu dropdown-user">
-        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+        <!-- <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
         </li>
         <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
         </li>
-        <li class="divider"></li>
+        <li class="divider"></li> -->
         <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
         </li>
     </ul>

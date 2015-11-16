6ba2862b8b6b24d3f13f3dbf80e46044831d7c4c
David Gibbs <ddave2122@gmail.com>
2015-11-16 02:24:47 +0000

    Resolved Merge conflict

--- website/include/getusers.php
+++ website/include/getusers.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<?php
-include_once('transporter.php');
-
-$transporter = new Transporter();
-$conn = $transporter->getConnection();
-
-// execute the stored procedure
-$sql = "SELECT first_name, last_name, id from user where is_manager = 0 and is_employee = 1;";
-$result = $stmt = $conn->query($sql);
-
-$resultSet = array();
-
-while($row = $result->fetch_assoc())
-{
-    $item = array();
-    $item[$row['id']] = $row['first_name'] . ' ' . $row['last_name'];
-    $resultSet[] = $item;
-}
-
-//$highLevelArray = array($resul);
-echo "{" . json_encode($resultSet) . "}";
-=======
 <?php
 include_once('transporter.php');
 
@@ -42,4 +19,3 @@ while($row = $result->fetch_assoc())
 
 
 echo json_encode($resultSet);
->>>>>>> 524595abf9a27e0a1b65d96b6b86513bac554f9e
